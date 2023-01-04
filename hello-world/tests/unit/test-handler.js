e44d3afea7a6bf5a544d2ff7162d5d4e2ec630bd
Antonio Galicia <antonio.galicia@sciencelogic.com>
2023-01-04 14:49:22 -0600

    Adding SAM CI/CD Pipeline definition again

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -16,7 +16,7 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello SAM");
+        expect(response.message).to.be.equal("hello SAM pipeline");
         // expect(response.location).to.be.an("string");
     });
 });
